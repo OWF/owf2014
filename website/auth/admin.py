f3c612683de06f90f9f08eabf5a60e37ec5bd192
Stefane Fermigier <sf@fermigier.com>
2014-09-27 11:20:08 +0200

    Final fix for csv exporter.

--- website/auth/admin.py
+++ website/auth/admin.py
@@ -1,9 +1,11 @@
 import StringIO
 import csv
+
 from flask import current_app, make_response
 from flask.ext.admin import expose
 from flask.ext.admin.contrib.sqla import ModelView
 from flask.ext.login import current_user
+from marshmallow import Serializer
 
 from .models import User2, Role, db
 
@@ -26,20 +28,20 @@ class UserModelView(SecureModelView):
   def csv(self):
     output = StringIO.StringIO()
     writer = csv.writer(output)
+
+    headers = [k for k in UserSerializer.Meta.fields]
+    writer.writerow(headers)
+
     for user in User2.query.all():
-      row = [
-        user.confirmed_at.strftime("%Y/%m/%d") if user.confirmed_at else "",
-        user.email.encode("utf8"),
-        user.first_name.encode("utf8"),
-        user.last_name.encode("utf8"),
-        user.gender.encode("utf8"),
-
-        user.title.encode("utf8"),
-        user.organization.encode("utf8"),
-        user.organization_type.encode("utf8"),
-
-        user.url.encode("utf8"), ]
+      d = UserSerializer(user).data
+      def encode(x):
+        if isinstance(x, unicode):
+          return x.encode('utf8')
+        else:
+          return str(x)
+      row = [encode(d[k]) for k in headers]
       writer.writerow(row)
+
     response = make_response(output.getvalue())
     response.headers['Content-Type'] = 'application/csv'
     return response
@@ -49,3 +51,9 @@ def register_admin_views(app):
   admin = app.extensions['admin'][0]
   admin.add_view(UserModelView(User2, db.session))
   admin.add_view(SecureModelView(Role, db.session))
+
+
+class UserSerializer(Serializer):
+  class Meta:
+    fields = ['confirmed_at', 'email', 'first_name', 'last_name', 'gender',
+              'title', 'organization', 'organization_type', 'url']
