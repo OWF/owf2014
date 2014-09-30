81f9f3023467153aeee0619dbe8ca782f333b53d
Stefane Fermigier <sf@fermigier.com>
2014-09-30 11:12:39 +0200

    Data dump.

--- website/views/api.py
+++ website/views/api.py
@@ -2,14 +2,18 @@ from flask import Blueprint, jsonify
 from markdown import markdown
 from marshmallow import Serializer, fields
 from sqlalchemy import func
+from website.auth import User2
 
-from website.crm.models import Talk, Speaker
+from website.crm.models import Talk, Speaker, Track2
 
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 route = blueprint.route
 
 
+#
+# Serializers
+#
 class TrackSerializer(Serializer):
   id = fields.Integer()
   name = fields.String()
@@ -48,6 +52,20 @@ class SpeakerSerializer(Serializer):
   talks = fields.Nested(TalkSerializer, many=True, only=('id', 'title',))
 
 
+class UserSerializer(Serializer):
+  id = fields.Integer()
+  name = fields.String()
+  first_name = fields.String()
+  last_name = fields.String()
+  title = fields.String()
+  organization = fields.String()
+  url = fields.String()
+  biography = fields.Function(lambda obj: markdown(obj.biography))
+
+
+#
+# Routes
+#
 @route('/talks')
 def talks():
   talks = Talk.query \
@@ -91,5 +109,31 @@ def speaker(id):
   return res
 
 
+@route('/all')
+def all():
+  talks = Talk.query \
+    .filter(Talk.starts_at != None) \
+    .order_by(Talk.starts_at).all()
+  serialized_talks = TalkSerializer(talks, many=True).data
+
+  speakers = Speaker.query.order_by(func.lower(Speaker.last_name)).all()
+  serialized_speakers = SpeakerSerializer(speakers, many=True).data
+
+  tracks = Track2.query.order_by(Track2.starts_at).all()
+  serialized_tracks = TrackSerializer(tracks, many=True).data
+
+  users = User2.query.order_by(User2.last_name, User2.first_name).all()
+  serialized_users = UserSerializer(users, many=True).data
+
+  res = jsonify(speaker=serialized_speakers,
+                talks=serialized_talks,
+                tracks=serialized_tracks,
+                users=serialized_users)
+  res.headers["Access-Control-Allow-Origin"] = "*"
+  return res
+
+
 def register(app):
   app.register_blueprint(blueprint)
+
+
